d6648abba5858fd51a60de27c346c86a28632a48
mar <marilirulita@gmail.com>
2022-04-06 14:00:04 -0700

    add spaces and change double quotes for single

--- morse_code.rb
+++ morse_code.rb
@@ -1,28 +1,24 @@
-$morse = {a: '.-', b: '-...', c: '-.-.', d: '-..', e: '.',
-f: '..-.', g: '--.', h: '....', i: '..', j: '.---', k: '-.-', l: '.-..', m: '--',
-n: '-.', o: '---', p: '.--.', q: '--.-', r: '.-.', s: '...', t: '-', u: '..-', v: '
-...-', w: '.--', x: '-..-', y: '-.--', z: '--..'}
-
 def decode_char(str)
-  $morse.each {|k, v| return k.upcase if v == str}
+  morse = { a: '.-', b: '-...', c: '-.-.', d: '-..', e: '.', f: '..-.', g: '--.', h: '....', i: '..', j: '.---', k: '-.-', l: '.-..', m: '--', n: '-.', o: '---', p: '.--.', q: '--.-', r: '.-.', s: '...', t: '-', u: '..-', v: '...-', w: '.--', x: '-..-', y: '-.--', z: '--..' }
+  morse.each { | k, v | return k.upcase if v == str }
 end
 
 def decode_word(str)
-  letters = str.split("\s")
-  solution = ""
-  letters.each do |l|
+  letters = str.split(' ')
+  solution = ''
+  letters.each do | l |
     solution += decode_char(l).to_s
   end
   solution
 end
 
 def decode_message(str)
-  words = str.split("   ")
-  solution = ""
-  words.each do |w|
-    solution += decode_word(w) + " "
+  words = str.split('   ')
+  solution = ''
+  words.each do | w |
+    solution += decode_word(w) + ' '
   end
   solution
 end
 
-puts decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
