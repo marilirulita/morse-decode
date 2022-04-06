3741955941ea076c813c6bc01e0653ce520af8ce
mar <marilirulita@gmail.com>
2022-04-06 14:02:35 -0700

    Remove unecesary spaces

--- morse_code.rb
+++ morse_code.rb
@@ -1,12 +1,12 @@
 def decode_char(str)
   morse = { a: '.-', b: '-...', c: '-.-.', d: '-..', e: '.', f: '..-.', g: '--.', h: '....', i: '..', j: '.---', k: '-.-', l: '.-..', m: '--', n: '-.', o: '---', p: '.--.', q: '--.-', r: '.-.', s: '...', t: '-', u: '..-', v: '...-', w: '.--', x: '-..-', y: '-.--', z: '--..' }
-  morse.each { | k, v | return k.upcase if v == str }
+  morse.each { |k, v| return k.upcase if v == str }
 end
 
 def decode_word(str)
-  letters = str.split(' ')
+  letters = str.split
   solution = ''
-  letters.each do | l |
+  letters.each do |l|
     solution += decode_char(l).to_s
   end
   solution
@@ -15,7 +15,7 @@ end
 def decode_message(str)
   words = str.split('   ')
   solution = ''
-  words.each do | w |
+  words.each do |w|
     solution += decode_word(w) + ' '
   end
   solution
